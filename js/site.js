3e0a735be8c49e4d4ef94e8f6175a88d02d57a09
Jason Wicker <jmwick01@gmail.com>
2024-03-12 21:43:15 -0400

    Added JS
    Added the JavaScript code to take a user's string, reverse it, and post it back to the page

--- js/site.js
+++ js/site.js
@@ -1,4 +1,29 @@
-//Call Hello World
-function helloWorld() {
-    alert("Hello World");    
+//get the string from the page
+//controller function
+function getValue() {
+    document.getElementById("alert").classList.add("invisible");
+    let userString = document.getElementById("userString").value;
+    let revString = reverseString(userString);
+    displayString(revString);
+}
+
+//reverse the string
+//logic function
+function reverseString(userString) {
+    let revString = [];
+
+    //reverse a string using a for loop
+    for (let index = userString.length - 1; index >= 0; index--) {
+        revString += userString[index];        
+    }
+    return revString;
+}
+
+//display the reversed string to the user
+//view function
+function displayString(revString) {
+    //write to the page
+    document.getElementById("msg").innerHTML = `Your string reversed is: ${revString}`;
+    //show the alert box
+    document.getElementById("alert").classList.remove("invisible");
 }
